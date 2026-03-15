367f3265a436d225514155160d28a9255dc6a765
zhyltsovd <zhyltsov@prolog.ltd>
2026-03-15 15:48:54 +0300

    spolier

--- src/Post.hs
+++ src/Post.hs
@@ -3,6 +3,7 @@
 
 module Post (Post, extractPost, savePost, showB) where
 
+import Control.Monad
 import Control.Monad.IO.Class
 
 import Data.ByteString.Lazy (ByteString)
@@ -17,7 +18,11 @@ import Data.Foldable
 
 import Text.HTML.TagSoup
 
-data PostElement = PostImage ByteString | PostLine ByteString | PostLineBreak
+data PostElement =
+  PostImage ByteString
+  | PostLine ByteString
+  | PostLineBreak
+  | PostSpoiler ByteString (Seq PostElement)
 
 showB :: ByteString -> String
 showB = T.unpack . T.decodeUtf8
@@ -27,15 +32,27 @@ instance Show PostElement where
   show (PostLine l) = showB l
   show PostLineBreak = "\n"
 
+data SpolierExtractor =
+  LookingForSpoilerTitle
+  | LookingForSpolier
+  | StopSpolier
+
+isSpolierExtracted :: SpolierExtractor -> Maybe (ByteString, Seq PostElement)
+isSpolierExtracted StopSpolier = undefined
+isSpolierExtracted _ = Nothing
+
 data PostExtractor =
   LookingForArticle
   | LookingForLines (Seq PostElement)
+  | Spoiler (Seq PostElement) SpolierExtractor
   | Stop (Seq PostElement)
+
+--  bbCodeSpoilerText
 
 valid :: BL.ByteString -> Bool
 valid bs =
-    not (BL.null bs)          -- 1) не пустой
-    && isValidUtf8 bs         -- 2) валидный UTF‑8
+    not (BL.null bs)
+    && isValidUtf8 bs
 
 isValidUtf8 :: BL.ByteString -> Bool
 isValidUtf8 = isRight . T.decodeUtf8'
@@ -48,17 +65,43 @@ isBBCodeImage attrs =
     then fmap PostImage $ lookup "src" attrs
     else Nothing
 
+isSpoilerStart :: [Attribute ByteString] -> Maybe ()
+isSpoilerStart attrs =
+  do
+    cls <- lookup "class" attrs
+    if "bbCodeSpoilerContainer" `elem` B.words (BL.toStrict cls)
+    then return ()
+    else mzero 
+    
+--  ToggleTriggerAnchor bbCodeSpoilerContainer
+
+-- SpoilerTarget bbCodeSpoilerText
+
 analyseImage :: [Attribute ByteString] -> Maybe PostElement
 analyseImage attrs = isBBCodeImage attrs
 
 -- bbCodeImage LbImage
 
+strans :: SpolierExtractor -> Tag ByteString -> SpolierExtractor
+strans = undefined
+
 trans :: PostExtractor -> Tag ByteString -> PostExtractor
+trans (Spoiler xs mach) tag =
+  let
+    mach' = strans mach tag
+  in
+    case isSpolierExtracted mach' of
+      Just (title, spoiler) -> LookingForLines (xs S.|> PostSpoiler title spoiler)
+      Nothing -> Spoiler xs mach'
 trans LookingForArticle (TagOpen "article" _) = LookingForLines S.empty
 trans (LookingForLines xs) (TagOpen "br" _) = LookingForLines (xs S.|> PostLineBreak)
 trans (LookingForLines xs) (TagOpen "img" attrs) =
   case analyseImage attrs of
     Just x -> LookingForLines (xs S.|> x)
+    Nothing -> LookingForLines xs
+trans (LookingForLines xs) (TagOpen "div" attrs) =
+  case isSpoilerStart attrs of
+    Just _ -> Spoiler xs LookingForSpoilerTitle
     Nothing -> LookingForLines xs
 trans (LookingForLines xs) (TagText x) | valid x = LookingForLines (xs S.|> PostLine x)
 trans (LookingForLines xs) (TagText x) = LookingForLines xs
